6280aec8175416811bbac78ea4402b7940419405
Premmalvatkar08 <premmalvatkar08@gmail.com>
2025-02-20 20:30:46 +0530

    Successfully ended with variables and data types

---
+++ OneDrive/Documents/Desktop/Python/Firstpage.py
@@ -0,0 +1,66 @@
+'''
+ Variable - A variable is a name given to a memory location in a program. -----------------------------------------------------
+
+print("Prem!!")
+print("My name is Prem Malvatkar.", "I am an Engineering student.")
+print(50+27)
+print("Hello prem")
+name = "Dreamer"
+age = 20
+price = 100.33
+old = True
+a = None
+
+print("my name is :", name)
+print("my age is :", age)
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+'''
+
+######## Key words ######## ---------------------------------------------------------------------------------------------------
+# keywords are reserved words in python
+
+# a = 50
+# b = 11
+# sum = a + b
+# print("The sum of two number is :- ", sum)
+
+# ''' Types of operators ------------------------------------------------------------------------------------------------------
+# 1. Arithematic operator :- (+, -, *, /, %(remainder), **(power))
+# 2. Relational operator :- (==, !=, >, <, >=, <=)
+# 3. Assignment operator :- (=, +=, -=, *=, /=, %=, **=)
+# 4. Logical Operator :- (not, And, OR).
+
+
+#Arithematic operator -----------------------------------------------------------------------------------------------------------
+# x = 10
+# y = 3
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y) (prints remainder)
+# print(x ** y) (power of)
+
+# Relational Operator -------------------------------------------------------------------------------------------------------
+# a = 25
+# b = 20
+# print(a==b)
+# print(a!=b)
+
+#Type conversion ------------------------------------------------------------------------------------------------------------
+# a = 3
+# b = 2.5
+# print(a + b) ...type conversion
+
+#input in python ---------------------------------------------------------------------------------------------------------
+
+#input() statement is used to accept values from user
+
+name = input("Enter your name :-")
+print("Hello ", name)
